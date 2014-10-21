5d63ae9068947153de864b0ae55da3f3f92d8926
Andrew Liu <dieseliu321@gmail.com>
2014-10-21 13:16:09 -0700

    Mobile Makers Day 1-2

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -13,7 +13,6 @@
 @property (weak, nonatomic) IBOutlet UILabel *multiplierLabel;
 @property (weak, nonatomic) IBOutlet UILabel *answerLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *operatorSegmentControl;
-@property (nonatomic) int valueFinal;
 
 @end
 
@@ -25,38 +24,43 @@
 
 //change multiplierLabel.text with UISlider
 - (IBAction)vauleChangedSlider:(UISlider *)sender {
-    self.multiplierLabel.text = [NSString stringWithFormat:@"%i", (int)sender.value];
+    self.multiplierLabel.text = [NSString stringWithFormat:@"%ld", (NSInteger)sender.value];
 }
 
-- (IBAction)onCalculateButtonPressed:(id)sender {
+- (IBAction)onCalculateButtonPressed:(UIButton *)sender {
 
 //retrieve numberTextField.text and convert it to an int
-    int valueNumber = self.numberTextField.text.intValue;
+    NSInteger valueNumber = self.numberTextField.text.integerValue;
 
 //retrieve multiplierLabel.text and convert it to an int
-    int valueMultiplier = self.multiplierLabel.text.intValue;
+    NSInteger valueMultiplier = self.multiplierLabel.text.integerValue;
 
 //Use different method to calculate the value of valueFinal based on the index of Segment
-    self.valueFinal = (self.operatorSegmentControl.selectedSegmentIndex == 0) ? valueNumber * valueMultiplier : valueNumber / valueMultiplier;
+    NSInteger valueFinal = (self.operatorSegmentControl.selectedSegmentIndex == 0) ? valueNumber * valueMultiplier : valueNumber / valueMultiplier;
 
 //assign the value of valueFinal to answerLabel.text
-    self.answerLabel.text = [NSString stringWithFormat:@"%i", (int)self.valueFinal];
+    self.answerLabel.text = [NSString stringWithFormat:@"%ld", valueFinal];
 
 //change view.backgroundColor based on the value of valueFinal
-    self.view.backgroundColor = (self.valueFinal >= 20.0) ? [UIColor greenColor] : [UIColor whiteColor];
+    self.view.backgroundColor = (valueFinal >= 20.0) ? [UIColor greenColor] : [UIColor whiteColor];
 
 //change answerLabel.text based on the value of valueFinal
-    BOOL valueIsMultipleOf5 = self.valueFinal >= 5 && self.valueFinal % 5 == 0;
-    BOOL valueIsMultipleOf3 = self.valueFinal >= 3 && self.valueFinal % 3 == 0;
-    if (valueIsMultipleOf5 && valueIsMultipleOf3){
+    BOOL valueIsMultipleOf5 = valueFinal >= 5 && valueFinal % 5 == 0;
+    BOOL valueIsMultipleOf3 = valueFinal >= 3 && valueFinal % 3 == 0;
+    if (valueIsMultipleOf5 && valueIsMultipleOf3)
+    {
         self.answerLabel.text = @"fizzbuzz";
     }
-    else if (valueIsMultipleOf5) {
-        self.answerLabel.text = @"buzz";
-    }
-    else if (valueIsMultipleOf3) {
-        self.answerLabel.text = @"fizz";
-    }
+    else
+        if (valueIsMultipleOf5)
+        {
+            self.answerLabel.text = @"buzz";
+        }
+        else
+            if (valueIsMultipleOf3)
+            {
+                self.answerLabel.text = @"fizz";
+            }
 
 //dismiss keyboard of numberTextField
     [self.numberTextField resignFirstResponder];
